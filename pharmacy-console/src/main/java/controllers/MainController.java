9abc50e6e9399ed9e1a85d0ef375682f2395e1aa
Stepan Repin <stnrepin@gmail.com>
2020-12-11 22:49:25 +0300

    course: Implement MainView logic

--- pharmacy-console/src/main/java/controllers/MainController.java
+++ pharmacy-console/src/main/java/controllers/MainController.java
@@ -1,4 +1,24 @@
 package controllers;
 
+import dao.hibernate.MedicineDao;
+import dao.hibernate.MedicineOrderDao;
+import javafx.fxml.FXML;
+import services.impl.MedicineOrderServiceImpl;
+import services.impl.MedicineServiceImpl;
+
 public class MainController {
+    @FXML MedicineController medicineController;
+    @FXML DiseasesController diseasesController;
+    @FXML MedicineOrderController medicineOrderController;
+
+    MedicineServiceImpl medicineService;
+    MedicineOrderServiceImpl medicineOrderService;
+
+    public void initialize() {
+        // TODO: inject
+        medicineService = new MedicineServiceImpl(new MedicineDao());
+        medicineOrderService = new MedicineOrderServiceImpl(new MedicineOrderDao());
+
+        medicineController.setMedicineService(medicineService);
+    }
 }
