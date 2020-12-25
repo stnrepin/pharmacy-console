67af7670800f1060a1e8afece08ce4cadc77010b
Stepan Repin <stnrepin@gmail.com>
2020-12-25 22:44:27 +0300

    gh-pages: Add docs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controllers"},{"l":"controllers.events"},{"l":"controllers.exceptions"},{"l":"dao"},{"l":"dao.hibernate"},{"l":"models"},{"l":"services"},{"l":"services.impl"},{"l":"utils"},{"l":"utils.event"}]
